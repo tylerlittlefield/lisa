303f18e8ff9379948c2e8cbb685afa5e6d21b5b3
tylurp <tylurp1@gmail.com>
2020-09-19 11:34:35 -0700

    updates :rocket:

--- tests/testthat/test-lisa-data.R
+++ tests/testthat/test-lisa-data.R
@@ -3,7 +3,7 @@ context("test-lisa-data")
 test_that("lisa data", {
 
   # confirm that lisa is a list and artwork is a data.frame
-  expect_true(class(lisa::lisa) == "list")
-  expect_true(class(lisa::artwork) == "data.frame")
+  expect_true(class(lisa) == "list")
+  expect_true(all(class(artwork) %in% c("data.frame", "tbl_df", "tbl")))
 
 })
